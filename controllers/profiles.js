054f9362076e6b330ec40bc218ea0aef57c15497
Andrew Galicia <andrewlgalicia@gmail.com>
2023-05-15 16:18:43 -0700

    fix initial login and user creation

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,7 +3,9 @@ const User = require('../models/user');
 
 async function show(req, res) {
   try {
+    console.log('show - req.user:', req.user);
     const user = await User.findById(req.user._id).populate('profile');
+    console.log('show - user:', user);
     res.render('profiles/show', { title: 'Profile new Page', profile: user.profile });
   } catch (err) {
     console.error(err);
@@ -16,18 +18,21 @@ function newProfile(req, res) {
 }
 
 async function createProfile(req, res) {
+  console.log('createProfile - req.user:', req.user);
   const user = await User.findById(req.user._id);
-  const newProfile = {
+  console.log('createProfile - user:', user);
+  const newProfile = new Profile({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     sleepGoal: {
-      hours: req.body.sleepGoal.hours,
-      minutes: req.body.sleepGoal.minutes
+      hours: req.body.sleepGoal ? req.body.sleepGoal.hours : 0,
+      minutes: req.body.sleepGoal ? req.body.sleepGoal.minutes : 0
     }
-  };
-  user.profile = newProfile;
+  });
+  await newProfile.save();
+  user.profile = newProfile._id;
   await user.save();
-  res.redirect(`/profiles/${user.profile._id}`);
+  res.redirect(`/profiles/${newProfile._id}`);
 }
 
 module.exports = {
